42d93e49ca0c78aa3d556296fe3282588b98da49
Isaac <izaroot327@gmail.com>
2021-04-14 09:17:41 -0400

    added catch

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+const bodyCon = document.querySelector("body")
+
+
+function submitData(nameStr, emailStr){
+     return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: nameStr,
+            email: emailStr
+        })
+    })
+    .then(resp => resp.json())
+    .then(json =>{
+        let idP = document.createElement("p")
+        idP.innerText = json.id
+        bodyCon.append(idP)
+    })
+
+    .catch(() => {
+        let idP = document.createElement("p")
+        idP.innerText = "Error"
+        bodyCon.append(idP)
+    })
+}
+
+// function deleteData(idNum){
+//     fetch(`http://localhost:3000/users/${idNum}`, {
+//         method: "DELETE"
+//     })
+// }
